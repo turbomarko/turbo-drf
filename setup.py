b35a5418644d6509d963bfa22fea61bed35c0f21
Mark Hoffman <7urbo.marko@gmail.com>
2021-12-11 21:43:54 +0100

    Cookiecutter 2021.11.18

--- setup.py
+++ setup.py
@@ -8,18 +8,11 @@ try:
 except ImportError:
     from distutils.core import setup
 
-# Our version ALWAYS matches the version of Django we support
-# If Django has a new release, we branch, tag, then update this setting after the tag.
-version = "3.1.13"
-
-if sys.argv[-1] == "tag":
-    os.system(f'git tag -a {version} -m "version {version}"')
-    os.system("git push --tags")
-    sys.exit()
+# We use calendar versioning
+version = "2021.11.17"
 
 with open("README.rst") as readme_file:
     long_description = readme_file.read()
-
 setup(
     name="cookiecutter-django",
     version=version,
@@ -34,7 +27,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
-        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "License :: OSI Approved :: BSD License",
