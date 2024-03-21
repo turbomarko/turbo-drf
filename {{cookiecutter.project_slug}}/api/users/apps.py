672530da4e0d4a265c69a61de6e2767065e0f7eb
Mark Hoffman <7urbo.marko@gmail.com>
2024-03-21 19:38:03 +0000

    Linting fixes

--- {{cookiecutter.project_slug}}/api/users/apps.py
+++ {{cookiecutter.project_slug}}/api/users/apps.py
@@ -1,12 +1,6 @@
-import contextlib
-
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
     name = "api.users"
     verbose_name = "Users"
-
-    def ready(self):
-        with contextlib.suppress(ImportError):
-            import api.users.signals  # noqa: F401
