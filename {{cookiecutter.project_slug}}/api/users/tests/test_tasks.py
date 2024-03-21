494e45b95c7fa01e484c34bcc241ef3fdd675529
Mark Hoffman <7urbo.marko@gmail.com>
2024-03-21 20:02:57 +0000

    Linting fixes

--- {{cookiecutter.project_slug}}/api/users/tests/test_tasks.py
+++ {{cookiecutter.project_slug}}/api/users/tests/test_tasks.py
@@ -1,8 +1,8 @@
 import pytest
 from celery.result import EagerResult
 
-from api.users.tasks import get_users_count
-from api.users.tests.factories import UserFactory
+from ..tasks import get_users_count
+from .factories import UserFactory
 
 pytestmark = pytest.mark.django_db
 
